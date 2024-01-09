f013c4c0a8715fa8c54df8ec07c5f0656f6362eb
kyl00h <raedaa.bou@gmail.com>
2024-01-09 10:00:39 +0100

    Implemented query parameters, improved comments and readability

--- martinator-api/Controllers/VideoCameraController.cs
+++ martinator-api/Controllers/VideoCameraController.cs
@@ -9,98 +9,87 @@ namespace martinator_api;
 [ApiController]
 public class VideoCameraController : ControllerBase
 {
-    // Data needed to access the camera recording, they are hardcoded for testing purposes
-    private string user = "admin";
-    private string password = "mutina23";
+    // Data needed to access the camera recording
+    string authenticationString = "admin:mutina23";
     private string ip = "93.57.67.110";
-    private string startTime = "00:00:01";
-    private string endTime = "00:00:60";
-    private string startDate = "2024-01-04";
-    private string endDate = "2024-01-04";
-    private HttpClient client = new HttpClient();
+
+    HttpClient client = new HttpClient();
 
 
-    [HttpGet("api/get-recording/")]
-    public async Task<IActionResult> GetRecording()
+    [HttpGet("api/saveRecording/")]
+    public async Task<IActionResult> SaveRecording([FromQuery] string startDate, string startTime, string endDate, string endTime) // Formatting: dates=yyyy-mm-dd  times=hh:mm:ss
     {
         // This method downloads a video from a camera.
         // It first retrieves the necessary information for the download request,
         // then sends the download request and checks if it was successful.
-
+    
         // TODO: Add error handling for the request and the curl command
-        // TODO: Set needed parameters as a query in the URL
+        // TODO: Add model validation for the query parameters
 
-
-        string authenticationString = $"{user}:{password}";
-        string recordInfoURL = $"http://{ip}/sdk.cgi?action=get.playback.recordinfo&chnid=0&stream=0&startTime={startDate}%20{startTime}&endTime={endDate}%20{endTime}";
+        // get.playback.recordinfo request
 
         // Setting Authentication header
         client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(
         "Basic", Convert.ToBase64String(
            Encoding.ASCII.GetBytes(authenticationString)));
 
-        // get.playback.recordinfo request
+        string recordInfoURL = $"http://{ip}/sdk.cgi?action=get.playback.recordinfo&chnid=0&stream=0&startTime={startDate}%20{startTime}&endTime={endDate}%20{endTime}";
+
         var recordInfoResponse = await client.GetAsync(recordInfoURL);
 
         if (recordInfoResponse.IsSuccessStatusCode)
         {
             var content = await recordInfoResponse.Content.ReadAsStringAsync();
 
-            // Parsing recordInfoResponse 
-            var lines = content.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
-            var recordInfoValues = new Dictionary<string, string>();
-
-            foreach (var line in lines)
-            {
-                var parts = line.Split('=');
-                if (parts.Length == 2)
-                {
-                    recordInfoValues[parts[0]] = parts[1];
-                }
-            }
+            // Parsing the content of recordInfoResponse 
+            Dictionary<string, string> recordInfoValues = Utility.ParseResponse(content);
 
             // Retrieving needed values for the get.playback.download request
+            // Available keys: cnt, sid, chnid, allCnt, allSize, ch0, recordType0, port0, lock0 (chapter 1.6.2 in Milesight documentation)
             string chnid = recordInfoValues["chnid"];
             string sid = recordInfoValues["sid"];
-            string size = recordInfoValues["allSize"];
+
             string recordDownloadURL = $"http://{ip}/sdk.cgi?action=get.playback.download&chnid={chnid}&sid={sid}&streamType=primary&videoFormat=mp4&streamData=1&startTime={startDate}%20{startTime}&endTime={endDate}%20{endTime}";
 
+            string fileName = $"NVR-CAM-S{Utility.FormatDate(startDate)}-{Utility.FormatTime(startTime)}-E{Utility.FormatDate(endDate)}-{Utility.FormatTime(endTime)}.mp4";
+
             // get.playback.recordinfo request through curl process
+
+            // Create a new ProcessStartInfo object
             var startInfo = new ProcessStartInfo
             {
+                // Set the FileName to "curl". This is the command that will be executed.
                 FileName = "curl",
-                Arguments = $"-o ./Data/recordings/NVR-CAM-S{formatDate(startDate)}-{formatTime(startTime)}-E{formatDate(endDate)}-{formatTime(endTime)}.mp4 -u admin:mutina23  {recordDownloadURL}",
+
+                // Set the Arguments for the curl command.
+                // -o specifies the output file path and name.
+                // -u specifies the username and password for authentication.
+                // The last argument is the URL from which the recording will be downloaded.
+                Arguments = $"-o ./Data/recordings/{fileName} -u admin:mutina23  {recordDownloadURL}",
+
+                // Set UseShellExecute to false. This means the process will be executed in the same process, not a new shell.
                 UseShellExecute = false,
             };
+
+            // Create a new Process object and set its StartInfo property to the previously created startInfo object
             var process = new Process { StartInfo = startInfo };
+
+            // Start the process. This will execute the curl command with the specified arguments.
             process.Start();
+
+            // Wait for the process to exit asynchronously. This is necessary to ensure that the process has completed
+            // its task (in this case, downloading the file) before the program continues. If we didn't wait, the program
+            // might try to use the file before it's fully downloaded, which would cause errors.
             await process.WaitForExitAsync();
 
-            return Ok("Video successfully downloaded");
-
-
+            return Ok("Video successfully downloaded"); // Not a good practice to check if everything went correctly, needs error handling
         }
+
         else
         {
-            return StatusCode((int)recordInfoResponse.StatusCode, recordInfoResponse.ReasonPhrase + "Unable to retrieve record info");
+            return StatusCode((int)recordInfoResponse.StatusCode, recordInfoResponse.ReasonPhrase + "Unable to retrieve recording info");
         }
 
-
     }
 
-    public string formatDate(string date)
-    {
-        // This method formats a date from yyyy-mm-dd to yyyymmdd
-        string[] dateParts = date.Split("-");
-        string formattedDate = dateParts[0] + dateParts[1] + dateParts[2];
-        return formattedDate;
-    }
-
-    public string formatTime(string time)
-    {
-        // This method formats a time from hh:mm:ss to hhmmss
-        string[] timeParts = time.Split(":");
-        string formattedTime = timeParts[0] + timeParts[1] + timeParts[2];
-        return formattedTime;
-    }
 }
